8771906db6a52fc3522d82e584f61fd44a5168a0
Daniel Pereira <pereiradpg@yahoo.ca>
2017-07-06 13:02:22 -0400

    09_HTML_STREAM now passes09_HTML_STREAM now passes09_HTML_STREAM now passes09_HTML_STREAM now passes09_HTML_STREAM now passes09_HTML_STREAM now passes09_HTML_STREAM now passes09_HTML_STREAM now passes09_HTML_STREAM now passes

--- 09_HTML_STREAM.js
+++ 09_HTML_STREAM.js
@@ -1,14 +1,21 @@
-var trumpet = require('trumpet')
-var through = require('through2')
-
-var to_upper = function (buffer) {
-  this.queue(buffer.toString().toUpperCase())
+/*var trumpet = require('trumpet')
+var through = require('through')
+var to_upper = function(buffer) {
+    this.queue(buffer.toString().toUpperCase())
 }
-
 var tr = trumpet()
 process.stdin.pipe(tr).pipe(process.stdout)
-
-// Writing to a trumpet element stream replaces the innerHtml of the matched
-// element.
 var stream = tr.select('.loud').createStream()
-stream.pipe(through(to_upper)).pipe(stream)
+stream.pipe(through(to_upper)).pipe(stream)*/
+
+var trumpet = require('trumpet');
+var through = require('through2');
+var tr = trumpet();
+  
+var loud = tr.select('.loud').createStream();
+loud.pipe(through(function (buf, _, next) {
+  this.push(buf.toString().toUpperCase());
+  next();
+})).pipe(loud);
+  
+process.stdin.pipe(tr).pipe(process.stdout);
